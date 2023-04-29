5710d784cd82757762f5e8be159757779f093139
Thiago3108 <santiago2234508@correo.uis.edu.co>
2023-04-29 09:13:37 -0400

    Se agrego una calculadora 2.0

--- desktop_app_01.py
+++ desktop_app_01.py
@@ -1,10 +1,37 @@
 # se import a la libreria tkinter con todas sus funciones
 from tkinter import *
+from tkinter import messagebox
 
 #------------------------------
 # funciones de la app
 #------------------------------
+def calcular():
+    messagebox.showinfo("Minicalculadora 2.0", "Hizo click en el boton calcular")
+    s = int(a.get()) + int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} + {b.get()} = {s}\n")
+    r = int(a.get()) - int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} - {b.get()} = {r}\n")
+    m = int(a.get()) * int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} * {b.get()} = {m}\n")
+    d = int(a.get()) / int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} / {b.get()} = {d}\n")
+    p = int(a.get()) ** int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} ** {b.get()} = {p}\n")
+    mod = int(a.get()) % int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} % {b.get()} = {mod}\n")
+    ent = int(a.get()) // int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()} // {b.get()} = {ent}\n")
 
+
+def borrar():
+    messagebox.showinfo("Minicalculadora 2.0", "Los datos seran borrados...")
+    a.set("")
+    b.set("")
+    t_resultados.delete("1.0", "end")
+
+def salir():
+    messagebox.showinfo("Minicalculadora 2.0", "La app se cerrara")
+    ventana_principal.destroy()
 #------------------------------
 #ventana principal
 #------------------------------
@@ -13,39 +40,93 @@ from tkinter import *
 ventana_principal = Tk()
 
 #titulo de la ventana
-ventana_principal.title("Sistemas uis socorro")
+ventana_principal.title("Minicalculadora 2.0")
 
 ventana_principal.geometry("500x500")
 
 ventana_principal.resizable(0,0)
 
 #color de fondo de la ventana
-ventana_principal.config(bg="medium sea green")
+ventana_principal.config(bg="gray")
 
+#------------------------------
+#variables de control
+#------------------------------
+a = StringVar()
+b = StringVar()
 #------------------------------
 #frame entrada datos
 #------------------------------
 
 frame_entrada = Frame(ventana_principal)
-frame_entrada.config(bg="black", width=500, height=250)
-frame_entrada.place(x = 0, y = 0)
+frame_entrada.config(bg="white", width=480, height=180)
+frame_entrada.place(x = 10, y = 10)
+
+#logo de la app
+#Label(frame_entrada, image=PhotoImage(file="img/logo_uis.png"), bg="white").place(x=11, y=40)
+"""
+logo = PhotoImage(file="img/logo_uis.png")
+label_logo = Label(frame_entrada, image=logo, bg="white")
+label_logo.place(x=11, y=40)
+"""
+#etiqueta titulo
+lb_titulo = Label(frame_entrada, text="Minicalculadora 1.0")
+lb_titulo.config(bg="white", fg="Green", font=("Helvetica", 20))
+lb_titulo.place(x=240, y = 10)
+
+#Etiqueta para el primer numero
+lb_a = Label(frame_entrada, text="a: ")
+lb_a.config(bg="white", fg="Green", font=("Helvetica", 20))
+lb_a.place(x = 250, y = 60)
+
+#caja de texto para el primer numero
+entry_a = Entry(frame_entrada, textvariable= a)
+entry_a.config(bg="white", fg="green", font=("Courier", 20))
+entry_a.focus_set()
+entry_a.place(x = 290, y = 60, width = 100, height=30)
+
+#Etiqueta para el segundo numero
+lb_b = Label(frame_entrada, text="b: ")
+lb_b.config(bg="white", fg="Green", font=("Helvetica", 20))
+lb_b.place(x = 250, y = 100)
+
+#caja de texto para el segundo numero
+entry_b = Entry(frame_entrada, textvariable= b)
+entry_b.config(bg="white", fg="green", font=("Courier", 20))
+entry_b.place(x = 290, y = 100, width = 100, height=30)
 
 #------------------------------
 #frame operaciones
 #------------------------------
 
 frame_operaciones = Frame(ventana_principal)
-frame_operaciones.config(bg="red", width=500, height=125)
-frame_operaciones.place(x = 0, y = 250)
+frame_operaciones.config(bg="white", width=480, height=100)
+frame_operaciones.place(x = 10, y = 200)
 
+#boton para calcular
+bt_calcular = Button(frame_operaciones, text = "Calcular", command=calcular)
+bt_calcular.place(x = 45, y=35, width= 100, height=30)
+
+#boton para borrar
+bt_borrar = Button(frame_operaciones, text = "Borrar", command=borrar)
+bt_borrar.place(x = 190, y=35, width= 100, height=30)
+
+#boton para salir
+bt_salir = Button(frame_operaciones, text = "Salir", command=salir)
+bt_salir.place(x = 335, y=35, width= 100, height=30)
 
 #------------------------------
 #frame resultados
 #------------------------------
 
 frame_resultados = Frame(ventana_principal)
-frame_resultados.config(bg="gold", width=500, height=125)
-frame_resultados.place(x = 0, y = 375)
+frame_resultados.config(bg="white", width=480, height=180)
+frame_resultados.place(x = 10, y = 310)
+
+#area de texto para resultados
+t_resultados = Text(frame_resultados)
+t_resultados.config(bg="black", fg = "green", font=("Courier-B", 15))
+t_resultados.place(x = 10, y = 10, width = 460, height=160)
 
 
 
